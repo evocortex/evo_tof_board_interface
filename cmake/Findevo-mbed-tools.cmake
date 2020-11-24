c519c8bb9b8704daea210a35b859cf5e5bfb0ae8
Jona Saffer <jona.saffer@evocortex.com>
2020-11-24 13:39:24 +0100

    fix cmake dependency management

--- cmake/Findevo-mbed-tools.cmake
+++ cmake/Findevo-mbed-tools.cmake
@@ -1,7 +1,7 @@
 include(FindPackageHandleStandardArgs)
 
-find_package(evo-mbed-tools NO_MODULE)
-if(DEFINED evo-mbed-tools_FOUND)
+find_package(evo-mbed-tools QUIET NO_MODULE)
+if(DEFINED evo-mbed-tools_FOUND AND evo-mbed-tools_FOUND)
 	find_package_handle_standard_args(evo-mbed-tools CONFIG_MODE)
 	return()
 endif()
@@ -11,11 +11,11 @@ find_path(EVO_MBED_TOOLS_INCLUDE NAMES evo_mbed
 	)
 
 if(DEFINED EVO_MBED_TOOLS_INCLUDE)
-	file(READ ${EVO_MBED_TOOLS_INCLUDE}/Version.h version_file)
+	file(READ "${EVO_MBED_TOOLS_INCLUDE}/evo_mbed/Version.h" version_file)
 
-	string(REGEX MATCH "EVO_MBED_TOOLS_VER\s+\"(\d\.\d\.\d)\"" _ ${version_file})
-	set(evo-mbed-tools_VERSION ${CMAKE_MATCH_1} CACHE INTERNAL)
-	set(evo-mbed-tools_INCLUDE_DIR ${EVO_MBED_TOOLS_INCLUDE} CACHE INTERNAL)
+	string(REGEX MATCH "EVO_MBED_TOOLS_VER[ \t]*\"([0-9]+.[0-9]+.[0-9]+)\"" _ ${version_file})
+	set(evo-mbed-tools_VERSION "${CMAKE_MATCH_1}")
+	set(evo-mbed-tools_INCLUDE_DIR "${EVO_MBED_TOOLS_INCLUDE}")
 endif()
 
 find_library(LIBEVO_MBED_TOOLS NAMES
@@ -29,14 +29,16 @@ find_library(LIBEVO_MBED_TOOLS NAMES
 if(DEFINED LIBEVO_MBED_TOOLS AND NOT TARGET evo-mbed-tools::evo-mbed-tools)
 	add_library(evo-mbed-tools::evo-mbed-tools UNKNOWN IMPORTED)
 	set_target_properties(evo-mbed-tools::evo-mbed-tools PROPERTIES
-		IMPORTED_LOCATION ${LIBEVO_MBED_TOOLS}
+		IMPORTED_LOCATION "${LIBEVO_MBED_TOOLS}"
 		)
 
-	set(evo-mbed-tools_LIBRARY ${LIBEVO_MBED_TOOLS} CACHE INTERNAL)
+	set(evo-mbed-tools_LIBRARY "${LIBEVO_MBED_TOOLS}")
 endif()
 
 find_package_handle_standard_args(evo-mbed-tools
-	REQUIRED_VARS evo-mbed-tools_INCLUDE_DIR evo-mbed-tools_LIBRARY
+	REQUIRED_VARS 
+		evo-mbed-tools_LIBRARY 
+		evo-mbed-tools_INCLUDE_DIR
 	VERSION_VAR evo-mbed-tools_VERSION
 	)
 
